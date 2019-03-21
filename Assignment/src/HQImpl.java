9c96123250e5241885ab4849d54faf31a1527b32
hamza-saeed <hamzasaeed@protonmail.com>
2019-03-21 14:39:09 +0000

    storing local server

--- Assignment/src/HQImpl.java
+++ Assignment/src/HQImpl.java
@@ -2,7 +2,18 @@ import CarPark.HQServerPOA;
 import CarPark.Machine;
 import CarPark.VehicleEvent;
 
+import java.util.ArrayList;
+
 public class HQImpl extends HQServerPOA {
+
+    public static ArrayList<Machine> listOfLocalServers;
+
+    public HQImpl()
+    {
+        listOfLocalServers = new ArrayList<Machine>();
+    }
+
+
     @Override
     public void raise_alarm(VehicleEvent event) {
 
@@ -11,5 +22,6 @@ public class HQImpl extends HQServerPOA {
     @Override
     public void register_local_server(Machine machine) {
         System.out.println(machine.name);
+        listOfLocalServers.add(machine);
     }
 }
