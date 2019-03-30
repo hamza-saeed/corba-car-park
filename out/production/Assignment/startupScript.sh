68558fe790b1f0bb97714f012c20978c574ca855
Hamza <hamzasaeed@protonmail.com>
2019-03-30 13:05:59 +0000

    major refactor and redesigned gui

--- out/production/Assignment/startupScript.sh
+++ out/production/Assignment/startupScript.sh
@@ -2,21 +2,35 @@ javac *.java
 
 java HQ -ORBInitialPort 1075 -LocalServers lserver001,lserver002,lserver003 &
 
-sleep .5
+sleep .4
 
-java LServer -ORBInitialPort 1075 -Name lserver003 -EntryGates entry001,entry002,entry003 -PayStations pay001,pay002,pay003 &
+java LServer -ORBInitialPort 1075 -Name lserver003 -EntryGates entry001,entry002 -PayStations pay001,pay002,pay003 -ExitGates exit001,exit002,exit003 &
 
-sleep .5
+sleep .4
+
+java LServer -ORBInitialPort 1075 -Name lserver001 -EntryGates entry003 -PayStations pay004 -ExitGates exit004 &
+
+sleep .4
 
 java EntryClient -ORBInitialPort 1075 -Name entry002 &
 
-sleep .5
+sleep .4
 
 java EntryClient -ORBInitialPort 1075 -Name entry003 &
 
-sleep .5
+sleep .4
 
-java PayStationClient -ORBInitialPort 1075 -Name pay003
+java PayStationClient -ORBInitialPort 1075 -Name pay003 &
+
+sleep .4
+
+java ExitClient -ORBInitialPort 1075 -Name exit001 &
 
 
+sleep .4
 
+java ExitClient -ORBInitialPort 1075 -Name exit002 &
+
+sleep .4
+
+java ExitClient -ORBInitialPort 1075 -Name exit004
